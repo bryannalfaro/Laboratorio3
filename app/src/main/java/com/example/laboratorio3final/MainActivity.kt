daf8deade078dccdf515ae473292b5b60a684d85
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-02-01 13:47:21 -0600

    Limpieza de historial de actividades y comentarios

--- app/src/main/java/com/example/laboratorio3final/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3final/MainActivity.kt
@@ -4,39 +4,66 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+
 import androidx.databinding.DataBindingUtil
 import com.example.laboratorio3final.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Creates the binding connection
     private lateinit var databinding2:ActivityMainBinding
-    private var tester:Boolean?=true
+    private var tester:Boolean?=true//Evaluate if the FloatingButton is selected
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
+
         databinding2= DataBindingUtil.setContentView(this,R.layout.activity_main)
         databinding2.star
         databinding2.star.setOnClickListener{
             switch_state()
         }
-        databinding2.button6
-        databinding2.button6.setOnClickListener {
+
+        databinding2.tikalButton
+        databinding2.tikalButton.setOnClickListener {
             val intent = Intent(this,Places::class.java)
             intent.putExtra("boton6","Tikal")
+
             startActivity(intent)
+
         }
-        databinding2.button7
-        databinding2.button7.setOnClickListener {
+        databinding2.antiguaButton
+        databinding2.antiguaButton.setOnClickListener {
             val intent = Intent(this,Places::class.java)
             intent.putExtra("boton7","Antigua")
+
             startActivity(intent)
+
         }
-        databinding2.button8
-        databinding2.button8.setOnClickListener {
+        databinding2.semucButton
+        databinding2.semucButton.setOnClickListener {
+
             val intent = Intent(this,Places::class.java)
             intent.putExtra("boton8","Semuc")
+
             startActivity(intent)
+
         }
+        //Get the data from the other activity
+        val string1: String? = intent.getStringExtra("comentario")
+        if(string1.isNullOrEmpty()){
+
+        }else{
+            Toast.makeText(this,string1,Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
+
+    /**
+     * Function that switch the state of the view
+     */
     private fun switch_state(){
         if(tester==true){
             var name=databinding2.editText2.text.toString()
@@ -48,10 +75,13 @@ class MainActivity : AppCompatActivity() {
             databinding2.nameSet.visibility= View.VISIBLE
             tester=false
         }else{
+
             databinding2.editText2.visibility= View.VISIBLE
             databinding2.textView3.visibility= View.VISIBLE
             databinding2.nameSet.visibility= View.GONE
             tester=true
         }
     }
+
+
 }
--- app/src/main/java/com/example/laboratorio3final/Places.kt
+++ app/src/main/java/com/example/laboratorio3final/Places.kt
@@ -11,28 +11,39 @@ import com.example.laboratorio3final.databinding.ActivityPlacesBinding
 import com.example.laboratorio3final.models.Modelo
 
 class Places : AppCompatActivity() {
+    //binding for this activity
     private lateinit var binding1:ActivityPlacesBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val prueba:Modelo=Modelo(getString(R.string.tikal), "ESTO ES SUBTITULO",getString(R.string.bio))
-        val prueba1:Modelo=Modelo(getString(R.string.antigua), "ESTO ES SUBTITULO","Descripcion")
-        val prueba2:Modelo=Modelo(getString(R.string.semuc), "ESTO ES SUBTITULO","Descripcion")
+        //Create the objects for the places
+        val prueba =Modelo(getString(R.string.tikal), getString(R.string.phrase1),getString(R.string.descripcion1))
+        val prueba1=Modelo(getString(R.string.antigua), getString(R.string.phrase2),getString(R.string.descripcion2))
+        val prueba2=Modelo(getString(R.string.semuc), getString(R.string.phrase3),getString(R.string.descripcion3))
 
         super.onCreate(savedInstanceState)
 
         binding1= DataBindingUtil.setContentView(this,
             R.layout.activity_places
         )
+        //Comment button
+        binding1.button5.setOnClickListener {
+            val intent=Intent(this,MainActivity::class.java)
+            intent.putExtra("comentario",binding1.editText.text.toString())
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
 
+        }
+        //Get the data from the other activity
         val string: String? = intent.getStringExtra("boton6")
         val string1: String? = intent.getStringExtra("boton7")
         val string2: String? = intent.getStringExtra("boton8")
 
+        //Evaluates the data
         if (string.equals("Tikal")){
             binding1.modelo=prueba
-            //Toast.makeText(this,strings,Toast.LENGTH_SHORT)
+
         }else if (string1.equals("Antigua")){
             binding1.modelo=prueba1
         }else if(string2.equals("Semuc")){
