c9f83a3428bf596ad2416119f3714e6fd78dc393
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-01-31 21:59:46 -0600

    Acceso a Resources

--- app/src/main/java/com/example/laboratorio3final/Places.kt
+++ app/src/main/java/com/example/laboratorio3final/Places.kt
@@ -2,32 +2,44 @@ package com.example.laboratorio3final
 
 
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.laboratorio3final.databinding.ActivityPlacesBinding
 import com.example.laboratorio3final.models.Modelo
 
 class Places : AppCompatActivity() {
     private lateinit var binding1:ActivityPlacesBinding
-    val prueba:Modelo=Modelo("ESTO FUNCIONO", "ESTO ES SUBTITULO","Descripcion")
-    val prueba1:Modelo=Modelo("ESTO FUNCIONO2", "ESTO ES SUBTITULO","Descripcion")
-    val prueba2:Modelo=Modelo("ESTO FUNCIONO3", "ESTO ES SUBTITULO","Descripcion")
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val prueba:Modelo=Modelo(getString(R.string.tikal), "ESTO ES SUBTITULO","Descripcion")
+        val prueba1:Modelo=Modelo("ESTO FUNCIONO2", "ESTO ES SUBTITULO","Descripcion")
+        val prueba2:Modelo=Modelo("ESTO FUNCIONO3", "ESTO ES SUBTITULO","Descripcion")
+
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main2)
+
         binding1= DataBindingUtil.setContentView(this,
             R.layout.activity_places
         )
 
-        val bundle: Bundle? = intent.extras
         val string: String? = intent.getStringExtra("boton6")
+        val string1: String? = intent.getStringExtra("boton7")
+        val string2: String? = intent.getStringExtra("boton8")
 
         if (string.equals("Tikal")){
             binding1.modelo=prueba
+            //Toast.makeText(this,strings,Toast.LENGTH_SHORT)
+        }else if (string1.equals("Antigua")){
+            binding1.modelo=prueba1
+        }else if(string2.equals("Semuc")){
+            binding1.modelo=prueba2
         }
+
+
 
 
 
